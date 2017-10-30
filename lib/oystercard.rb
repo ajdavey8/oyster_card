03a68e7a4a58845e3ffc8feea245e7b4c4e3f6d0
Andrew Davey <ajdavey8@gmail.com>
2017-10-30 18:53:19 +0000

    Implemented touch out, in journey and card minimum

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,14 +2,16 @@ class Oystercard
 
   DEFAULT_BALANCE = 0
   LIMIT = 90
+  MINIMUM = 1
   attr_reader :balance
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(amount)
-    error = "Can't top-up over the balance limit of £#{Oystercard::LIMIT}"
+    error = "Can't top-up over the balance limit of £#{LIMIT}"
     raise error if limit?(amount)
     @balance += amount.to_i
   end
@@ -19,12 +21,27 @@ class Oystercard
   end
 
   def touch_in
-    true
+    raise "Sorry insufficent funds for journey"  if sufficent_funds?
+    raise "Can't touch in if in journey" if in_journey?
+    @in_journey = true
+  end
+
+  def touch_out
+    raise "Can't touch out if not touched in" unless in_journey?
+    @in_journey = false
   end
 
   private
 
   def limit?(amount)
-    @balance + amount.to_i > Oystercard::LIMIT
+    @balance + amount.to_i > LIMIT
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def sufficent_funds?
+    @balance < MINIMUM
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:oystercard) { described_class.new }
+  let(:oystercard_with_money) { Oystercard.new(5) }
 
   it 'Returns the default 0 balance of oystercard' do
     expect(oystercard.balance).to eq Oystercard::DEFAULT_BALANCE
@@ -22,6 +23,24 @@ describe Oystercard do
   end
 
   it 'Confirm the oyster is touched in' do
-    expect(oystercard.touch_in).to eq true
+    expect(oystercard_with_money.touch_in).to eq true
+  end
+
+  it 'Confirm the oyster is touched out' do
+    oystercard_with_money.touch_in
+    expect(oystercard_with_money.touch_out).to eq false
+  end
+
+  it 'raises an error if trying to touch in when already in journey' do
+    oystercard_with_money.touch_in
+    expect { oystercard_with_money.touch_in }.to raise_error "Can't touch in if in journey"
+  end
+
+  it 'raises an error if trying to touch in when already in journey' do
+    expect { oystercard.touch_out }.to raise_error "Can't touch out if not touched in"
+  end
+
+  it 'raises an error if touched in with insufficent funds' do
+    expect { oystercard.touch_in}.to raise_error "Sorry insufficent funds for journey"
   end
 end
