ab5f4cb8f8227d1658f16ac11f3c2e075ae29d4a
Oleg Grushetskyy <olegfkl@gmail.com>
2017-10-30 15:41:54 +0000

    Create instance variable of default oystercard balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,9 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) {described_class.new}
+
+  it "Returns the default 0 balance of oystercard" do
+  expect(oystercard.balance).to eq 0
+  end
 end
