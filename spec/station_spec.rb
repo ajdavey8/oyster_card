335ddbf8834ff4a0b6830df205ad30f45b11563a
Allan Wazacz <cazwazacz@gmail.com>
2017-10-31 15:33:58 +0000

    Creates station class and adds test for name and zone

---
+++ spec/station_spec.rb
@@ -0,0 +1,15 @@
+require 'station'
+
+describe Station do
+  describe "#initialize" do
+    context "when station is initialized" do
+      station = Station.new('Euston', 1)
+      it 'shows Euston as name' do
+        expect(station.name).to eq('Euston')
+      end
+      it 'shows 1 as zone number' do
+        expect(station.zone).to eq(1)
+      end
+    end
+  end
+end
